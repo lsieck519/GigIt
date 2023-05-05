556c255e710568afafb0c3526bae5812b0c7e59a
ndallich1 <ndallich@gmail.com>
2023-05-04 21:17:13 -0400

    added todo's and more comments - trying to get server running
    Co-authored-by: cxrstings <cxrstings@users.noreply.github.com>
    Co-authored-by: Mary Webby <marywebby@users.noreply.github.com>
    Co-authored-by: Laura Sieck <lsieck519@users.noreply.github.com>

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,38 @@
+// import React from "react";
+// import Auth from "../../utils/auth";
+// import { Link } from "react-router-dom";
+
+// function Nav() {
+
+//   function showNavigation() {
+//     if (Auth.loggedIn()) {
+//       return (
+//         <ul className="">
+//           <li className="">
+//             <Link to="/profile">
+//               GigProfile
+//             </Link>
+//           </li>
+//           <li className="">
+//             <a href="/" onClick={() => Auth.logout()}>
+//               Logout
+//             </a>
+//           </li>
+//         </ul>
+//       );
+//     } else {
+//       return (
+//         <ul className="">
+//           <li className="">
+//             <Link to="/">
+//               Get Started
+//             </Link>
+//           </li>
+//         </ul>
+//       );
+//     }
+//   }
+
+// }
+
+// export default Nav;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,4 @@
+// importing gql from our apollo client
+import { gql } from "@apollo/client";
+
+// TODO: Add queries
