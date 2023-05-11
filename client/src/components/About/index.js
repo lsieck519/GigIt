dacb8741389d78e67e9cb7b662a0e6af4c49d579
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-05-11 17:39:25 -0500

    css and about

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -29,7 +29,17 @@ const About = ({ userId, loggedInUserId }) => {
   };
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (error) return (
+    <p className="error">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. {error.message}
+    </p>
+  );
 
   const { firstName, lastName } = data.user;
   const isCurrentUser = userId === loggedInUserId;
