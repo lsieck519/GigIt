e2b1484e42ebeb300a8a10892cbba0f64a6a3df0
ndallich1 <ndallich@gmail.com>
2023-05-13 18:07:53 -0400

    added logo to footer - footer complete

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer style={{ position: 'fixed', left: 0, bottom: 0, width: '100%', backgroundColor: '#f5f5f5', textAlign: 'center', padding: '10px' }}>
-      <p className='footerText'>© 2023 GigIt</p>
+    <footer
+      style={{
+        position: "fixed",
+        left: 0,
+        bottom: 0,
+        width: "100%",
+        backgroundColor: "#f5f5f5",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      <p className="footerText is-flex is-justify-content-center is-align-items-center">
+        © 2023{" "}
+        <img src={`/images/GigIt.png`} alt="gigit logo" class="footer-logo" />
+      </p>
     </footer>
   );
-}
+};
 
 export default Footer;
