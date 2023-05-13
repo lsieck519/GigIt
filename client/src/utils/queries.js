030a3af74a8a38c0c7c63f1c2437ef74fa79a00a
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-05-12 20:00:16 -0500

    about section still isnt working but i redid mutations after rewatching lectures and i went ahead and updated resolvers and typedefs for those mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,45 +1,8 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 // get all gigs to display in tiles on user's profile page
-export const GET_USER = gql`
-  query getUser {
-    me {
-      gigs {
-        _id
-        title
-        description
-      }
-      socials {
-        linkedIn
-        instagram
-        facebook
-        twitter
-        github
-        stackOverflow
-      }
-    }
-  }
-`;
 
-
-
-export const GET_USER_GIGS = gql`
-  query getUserGigs {
-    me {
-      gigs {
-        _id
-        title
-        description
-        image
-        compensation
-        yearsExperience
-      }
-    }
-  }`;
-
-  // new query for Gig Profiles
-  //TODO: Fix this
-  export const GET_USER_PROFILE = gql`
+export const GET_USER_PROFILE = gql`
   query getUserProfile {
     me {
       _id
@@ -66,18 +29,4 @@ export const GET_USER_GIGS = gql`
       }
     }
   }
-  `
-
-// get specific gig
-// export const GET_GIG = gql`
-//   query getGig($gigId: ID!) {
-//     gig(id: $gigId) {
-//       _id
-//       title
-//       description
-//       image
-//       compensation
-//       yearsExperience
-//     }
-//   }
-// `;
+`;
